bbaf4e272b6be0245e5554d1ec4f974ff7716ecb
Rafael Ceron <rrfcero98@gmail.com>
2022-12-28 18:43:25 -0600

    Exportando e importando elemento Testimonio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Testimonio />
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -12,8 +12,9 @@ function Testimonio() {
         <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
         <p className='cargo-testimonio'>Ingeniera de Software en Sportify</p>
         <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
-        
       </div>
     </div>
   );
 }
+
+export default Testimonio;
