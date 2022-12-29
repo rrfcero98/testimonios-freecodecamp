b67500c498514abf102cefd131cdc3b4c0c26fbe
Rafael Ceron <rrfcero98@gmail.com>
2022-12-29 16:29:26 -0600

    Agregando palabras en negritas

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import '../hojas-de-estilo/Testimonio.css';
+import React from "react";
+import "../hojas-de-estilo/Testimonio.css";
 
 export function Testimonio(props) {
   return (
-    <div className = 'contenedor-testimonio'>
-      <img 
-        className = 'imagen-testimonio'
-        src = {require(`../imagenes/perfil-${props.imagen}.png`)}
-        alt = 'foto-perfil'
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require(`../imagenes/perfil-${props.imagen}.png`)}
+        alt="foto-perfil"
       />
-      <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
-        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
-        <p className='texto-testimonio'>{props.testimonio}</p>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">
+          {props.testimonio}
+        </p>
       </div>
     </div>
   );
