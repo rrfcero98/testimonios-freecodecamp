dd6cee9239eac92e77b214928e2f9bea13bd713e
Rafael Ceron <rrfcero98@gmail.com>
2022-12-28 18:17:23 -0600

    Agregando primera parte de la estructura del testimonio con imagen sin usar prompt, agregando también carpeta de imagenes

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Testimonio() {
+  return (
+    <div className = 'contenedor-testimonio'>
+      <img 
+        className = 'imagen-testimonio'
+        src = {require('../imagenes/cara-1.jpg')}
+        alt = 'foto-perfil-1'
+      />
+    </div>
+  );
+}
